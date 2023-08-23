6e904dc1416ddce94024af6eee89572d12a7a8ee
Lakshay Jain <lakshayjain@Jayas-MacBook-Pro.local>
2023-08-23 14:42:39 +0530

    Added Dependent and some minor changes

--- app/models/task_category.rb
+++ app/models/task_category.rb
@@ -1,4 +1,4 @@
 class TaskCategory < ApplicationRecord
-  has_many :task_task_categories
+  has_many :task_task_categories, dependent: :destroy
   has_many :tasks, through: :task_task_categories
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ApplicationRecord
-  enum role: {admin: 0, normal: 1}
-  has_many :tasks, foreign_key: 'assignee_user_id'
-  has_many :task_comments
+  enum role: { admin: 0, normal: 1 }
 
+  has_many :tasks, foreign_key: 'assignee_user_id', dependent: :destroy
+  has_many :task_comments, dependent: :destroy
+
+  validates_presence_of :phone_number, :name, :password
   validates :email, presence: true, uniqueness: true
-  validates :phone_number, presence: true
-  validates :name, presence: true
-  validates :password, presence: true
   validates :active, inclusion: { in: [true, false] }
 end
