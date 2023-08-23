fc661f4bc76b5b7e070c0c6b58262e72f57c2a4e
Lakshay Jain <lakshayjain@Jayas-MacBook-Pro.local>
2023-08-23 15:42:44 +0530

    Added auth_token logic in Users model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,8 @@
 class Task < ApplicationRecord
   enum status: { pending: 1, in_progress: 2, completed: 3 }
 
-  belongs_to :assignee_user, foreign_key: 'assignee_user_id', class_name: 'User'
-  belongs_to :assigner_user, foreign_key: 'assigner_user_id', class_name: 'User'
+  belongs_to :assignee_user, class_name: 'User'
+  belongs_to :assigner_user, class_name: 'User'
 
   has_many :task_comments, dependent: :destroy
   has_many :task_task_categories, dependent: :destroy
--- app/models/task_category.rb
+++ app/models/task_category.rb
@@ -1,4 +1,6 @@
 class TaskCategory < ApplicationRecord
   has_many :task_task_categories, dependent: :destroy
   has_many :tasks, through: :task_task_categories
+
+  validates_presence_of :name, :description
 end
