33a07bd7ae35d4fb616c9f5db560c4d76eb80f40
Lakshay Jain <lakshayjain@Jayas-MacBook-Pro.local>
2023-08-24 12:40:51 +0530

    Added Default Task Category

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+TaskCategory.create!(name: "default_category", description: "description")
