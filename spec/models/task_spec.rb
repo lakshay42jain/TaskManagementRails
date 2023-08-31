d6effba3aaa33ce4f18f15167894cb2de9c929b9
Lakshay Jain <lakshayjain@192.168.1.6>
2023-08-31 21:57:00 +0530

    Add Validation Presnece of Attributes

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,48 +3,76 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
   let(:user) { FactoryBot.create(:user) }
   let(:admin) { FactoryBot.create(:user, role: 0, email: 'admin@gmail.com') }
-  let(:task) { FactoryBot.create(:task, assigner_user_id: admin.id)}
+  let(:task) { FactoryBot.create(:task, assigner_user_id: admin.id) }
 
-  it 'assigns default category before creation' do
-    task = FactoryBot.create(:task, assignee_user_id: user.id, assigner_user_id: admin.id)
-    default_category = TaskCategory.find_by(name: 'Default')
-    expect(task.categories).to include(default_category)
+  context 'Validation presence of attributes' do
+    it 'validates presence of title' do
+      expect(task).to validate_presence_of(:title)
+    end
+
+    it 'validates presence of description' do
+      expect(task).to validate_presence_of(:description)
+    end
+
+    it 'validates presence of due_date' do
+      expect(task).to validate_presence_of(:due_date)
+    end
+
+    it 'validates presence of status' do
+      expect(task).to validate_presence_of(:status)
+    end
+
+    it 'validates presence of priority' do
+      expect(task).to validate_presence_of(:priority)
+    end
   end
 
-  it 'cannot be created with completed status' do
-    task = Task.new(assigner_user_id: admin.id, assignee_user_id: user.id, status: 4)
-    expect(task).not_to be_valid
+  context 'before creation validate' do 
+    it 'assigns default category before creation' do
+      task = FactoryBot.create(:task, assignee_user_id: user.id, assigner_user_id: admin.id)
+      default_category = TaskCategory.find_by(name: 'Default')
+      expect(task.categories).to include(default_category)
+    end
   end
 
-  it 'task not created by normal user' do
-    task = Task.new(assigner_user_id: user.id, assignee_user_id: user.id)
-    expect(task).not_to be_valid
+  context 'wrong attributes values' do 
+    it 'cannot be created with completed status' do
+      task = Task.new(assigner_user_id: admin.id, assignee_user_id: user.id, status: 4)
+      expect(task).not_to be_valid
+    end
+  
+    it 'task not created by normal user' do
+      task = Task.new(assigner_user_id: user.id, assignee_user_id: user.id)
+      expect(task).not_to be_valid
+    end
+  
+    it 'validates due date format and range' do
+      task = Task.new(assigner_user_id: admin.id, assignee_user_id: user.id, due_date: 'invalid_date')
+      expect(task).not_to be_valid
+  
+      task.due_date = Date.yesterday
+      expect(task).not_to be_valid
+    end
   end
 
-  it 'validates due date format and range' do
-    task = Task.new(assigner_user_id: admin.id, assignee_user_id: user.id, due_date: 'invalid_date')
-    expect(task).not_to be_valid
+  context 'Assosciations Validate' do 
+    it 'associates with assignee and assigner users' do
+      task = FactoryBot.create(:task, assignee_user_id: user.id, assigner_user_id: admin.id)
+      expect(task.assignee).to eq(user)
+      expect(task.assigner).to eq(admin)
+    end
 
-    task.due_date = Date.yesterday
-    expect(task).not_to be_valid
-  end
+    it 'associates with comments' do
+      task = FactoryBot.create(:task, assignee_user_id: user.id, assigner_user_id: admin.id)
+      comment = FactoryBot.create(:task_comment, task_id: task.id, user_id: user.id)
+      expect(task.comments).to include(comment)
+    end
 
-  it 'associates with assignee and assigner users' do
-    task = FactoryBot.create(:task, assignee_user_id: user.id, assigner_user_id: admin.id)
-    expect(task.assignee).to eq(user)
-    expect(task.assigner).to eq(admin)
-  end
-
-  it 'associates with comments' do
-    task = FactoryBot.create(:task, assignee_user_id: user.id, assigner_user_id: admin.id)
-    comment = FactoryBot.create(:task_comment, task_id: task.id, user_id: user.id)
-    expect(task.comments).to include(comment)
-  end
-
-  it 'associates with categories through task_task_categories' do
-    task = FactoryBot.create(:task, assignee_user_id: user.id, assigner_user_id: admin.id)
-    category = FactoryBot.create(:task_category)
-    task.categories << category
-    expect(task.categories).to include(category)
+    it 'associates with categories through task_task_categories' do
+      task = FactoryBot.create(:task, assignee_user_id: user.id, assigner_user_id: admin.id)
+      category = FactoryBot.create(:task_category)
+      task.categories << category
+      expect(task.categories).to include(category)
+    end
   end
 end
